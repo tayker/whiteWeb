675b2632e5f1bc4d6192c9dadbee0b63a23bf21b
tayker <ser.mazur501@gmail.com>
2018-02-22 17:06:53 +0200

    fixed defects

--- dist/js/script.js
+++ dist/js/script.js
@@ -2,9 +2,19 @@ $(document).ready(function () {
     $('#mainSlider').slick({
         prevArrow: false,
         nextArrow: false,
-        autoplay: true,
-        autoplaySpeed: 3000
+        swipe: false,
     });
+    
+    $('.main .menu a[data-slide]').click(function(e) {
+        e.preventDefault();
+        $('.main .menu a').removeClass('active');
+        let slideNum = $(this).data('slide');
+        
+        $('#mainSlider').slick('slickGoTo', slideNum - 1);
+        
+        $('.main .menu a[data-slide="' + slideNum + '"]').addClass('active');
+    });
+    
     $('#navBtn').click(function () {
         $('#nav').toggleClass('active');
     })
